a487a015a45b790d9c7c14af17a428da896abd59
Interst3llarYT <krish.gopalk736@gmail.com>
2026-06-26 22:36:41 -0400

    Setup and created backend

---
+++ app.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import type { Request, Response } from "express";
+const app = express();
+app.get("/", (req: Request, res: Response) => {
+    res.send("backend started");
+    console.log("app was running");
+});
+
+app.listen(8080, () =>{
+    console.log("app is running");
+}
+);
+console.log("end of the program");
